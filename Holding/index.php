035ff312f3bca7d931ff414d742891a27ff8eef8
SalNorth <sithlord.north@gmail.com>
2014-03-12 23:10:29 +0000

    Custom "in development page"
    Customised development text for deadbeatdirty

--- Holding/index.php
+++ Holding/index.php
@@ -1,5 +1,16 @@
-<title><?=$_SERVER['HTTP_HOST']?> is currently under construction</title>
-<font face="tahoma, arial, helvetica, sans-serif">
-<p>Welcome to <strong><?=$_SERVER['HTTP_HOST']?></strong>, a site recently created using our clustered <?=PHP_OS?> web hosting.</p>
-<p>If you're seeing this page instead of your website, please replace or remove the default index.php from your public_html directory.</p>
-</font>
+<!-- <title><?=$_SERVER['HTTP_HOST']?> is currently under construction</title>
+ <font face="tahoma, arial, helvetica, sans-serif">
+ <p>Welcome to <strong><?=$_SERVER['HTTP_HOST']?></strong>, a site recently created using our clustered <?=PHP_OS?> web hosting.</p>
+ <p>If you're seeing this page instead of your website, please replace or remove the default index.php from your public_html directory.</p>
+ </font>
+-->
+<html>
+<head>
+<title>Deadbeat Dirty</title>
+</head>
+<body>
+<h1>Deadbeat Dirty</h1>
+<p>www.deadbeatdirty.co.uk is currently under construction.</p>
+<p>Contact details will be available soon</p>
+</body>
+</html>
